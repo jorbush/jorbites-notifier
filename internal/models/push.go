62ba155d2562e5ac8ab5ec20878860490ebdfa78
Jordi <55429631+jorbush@users.noreply.github.com>
2026-02-01 12:07:14 +0100

    fix object id

--- internal/models/push.go
+++ internal/models/push.go
@@ -8,7 +8,7 @@ import (
 
 type PushSubscription struct {
 	ID        bson.ObjectID `bson:"_id,omitempty" json:"id"`
-	UserID    string        `bson:"userId" json:"userId"`
+	UserID    bson.ObjectID `bson:"userId" json:"userId"`
 	Endpoint  string        `bson:"endpoint" json:"endpoint"`
 	P256dh    string        `bson:"p256dh" json:"p256dh"`
 	Auth      string        `bson:"auth" json:"auth"`
